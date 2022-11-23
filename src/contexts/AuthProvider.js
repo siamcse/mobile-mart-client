02ac16495dba16a8a9b0fcc681b8a106f6eeb91b
Abu Siam <siamcse01@gmail.com>
2022-11-24 00:22:53 +0600

    contexts added

--- src/contexts/AuthProvider.js
+++ src/contexts/AuthProvider.js
@@ -1,11 +1,21 @@
-import React, { createContext } from 'react';
-import { getAuth } from 'firebase/auth';
+import React, { createContext, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import app from '../firebase/firebase.config';
 
 export const AuthContext = createContext();
 const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState({});
+    const [loading, setLoading] = useState(true);
+
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    };
+    const login = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password);
+    }
     const authInfo = {
 
     }
